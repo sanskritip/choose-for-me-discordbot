d988038a4cde99744ebcd4859359e03648d47cf5
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-05-29 23:33:07 +0530

    Added Help and Some minor fixes

--- index.js
+++ index.js
@@ -24,17 +24,31 @@ bot.on('message', msg => {
       msg.channel.fetchMessages().then(messages => {
         finalArray = []
         messages.array().forEach(message => {
-          let msgcontent = message.content.split(" ",2);
+          let msgcontent = message.content.split(" ");
+          //console.log(msgcontent)
           if(message.author.id != '847848403479691316' && msgcontent[0]=="!"+choice)
                 {
-                  finalArray.push(msgcontent[1])
-                }
+                  msgcontent.shift()
+                  finalArray.push(msgcontent.join(" "))
+                }     
         });
-        //console.log(finalArray)
-        msg.reply(finalArray[utils.randomNumber(finalArray.length)])
+        let len = finalArray.length
+        if(len==0)
+          msg.reply(`Oh boi!😔 No ${choice} found! Maybe add something to your list by using **!${choice} <ENTRY>** ?`)
+        else
+          msg.reply(finalArray[utils.randomNumber(len)])
     })
+      break;
     }
-  
+    case "help" :
+    case "commands" : {
+      msg.channel.send(utils.command_list.map((i) => `✨ **${i[0]}** : *${i[1]}* `).join("\n"))
+      break;
+    }
+    case "about" : {
+      msg.channel.send("The **ChooseForMe Bot** 🍿 says Hi! \n I help you make bucket lists on the Server Channel and helps you make spontaneous plans on the go 🚀")
+      break;
+    }
   }
   /*
   if(content){
--- utils.js
+++ utils.js
@@ -2,6 +2,9 @@ function randomNumber(num){
     return Math.floor(Math.random() * num)
 }
 
+command_list = [['!choose <LISTNAME>','Chooses random entry from LISTNAME'],['!<LISTNAME> <ENTRY>','Adds Entry to List'],['!commands','Returns all valid bot commands'],['!about','Gives a short description about the bot.']]
 module.exports = {
-    randomNumber : randomNumber
+    randomNumber : randomNumber,
+    command_list : command_list
 };
+
