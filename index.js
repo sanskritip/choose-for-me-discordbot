f71713e528ecf92f5eebfd34fa5682021276355e
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-05-29 21:51:52 +0530

    Added Choose Command

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+
+const utils = require('./utils');
 
 bot.login(TOKEN);
 
@@ -11,7 +14,30 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if(msg.content == "choose"){
+  if(msg.content[0]!='!')
+    return;
+  let content = msg.content.substring(1).trim().toLowerCase();
+  var msgtok = content.split(" ");
+  switch(msgtok[0]){
+    case "choose" : {
+      let choice = msgtok[1]
+      msg.channel.fetchMessages().then(messages => {
+        finalArray = []
+        messages.array().forEach(message => {
+          let msgcontent = message.content.split(" ",2);
+          if(message.author.id != '847848403479691316' && msgcontent[0]=="!"+choice)
+                {
+                  finalArray.push(msgcontent[1])
+                }
+        });
+        //console.log(finalArray)
+        msg.reply(finalArray[utils.randomNumber(finalArray.length)])
+    })
+    }
+  
+  }
+  /*
+  if(content){
     msg.channel.fetchMessages().then(messages => {
       finalArray = []
       messages.array().forEach(message => {
@@ -20,8 +46,8 @@ bot.on('message', msg => {
                 finalArray.push(message.content)
               }
       });
-      //console.log(finalArray)
       msg.reply(finalArray[Math.floor(Math.random() * finalArray.length)])
   })
   }
+  */
 });
---
+++ utils.js
@@ -0,0 +1,7 @@
+function randomNumber(num){
+    return Math.floor(Math.random() * num)
+}
+
+module.exports = {
+    randomNumber : randomNumber
+};
